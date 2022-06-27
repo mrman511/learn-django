61990f839c9932adec8d8cf9697e5ae6bc83c535
Paul Bodner <bodner80@gmail.com>
2022-06-27 14:01:59 -0700

    Udemy Python Django complete course section 6 ch 14. Installed themes and styling for main, navbar, projects, project, project-form

--- devsearch/projects/forms.py
+++ devsearch/projects/forms.py
@@ -1,8 +1,23 @@
 from django.forms import ModelForm
+from django import forms
 from .models import Project
 
 class ProjectForm(ModelForm):
   class Meta:
     model = Project
     fields = ['title', 'featured_image', 'description', 'demo_link', 'source_link', 'tags' ]
+    widgets ={ 
+      'tags': forms.CheckboxSelectMultiple(),
+    }
+    
+
+  def __init__(self, *args, **kwargs):
+    super(ProjectForm, self).__init__(*args, **kwargs)
+    # self.fields['title'].widget.attrs.update({'class': 'input', 'placeholder': 'Add Title'})
+    # self.field['description'].widget.attrs.update({'class': 'input', 'placeholder': 'Add Description'})
+    # self.field['description'].widget.attrs.update({'class': 'input', 'placeholder': 'Add Description'})
+
+    for name, field in self.fields.items():
+      field.widget.attrs.update({'class': 'input'})
+
     #fields = '__all__'
