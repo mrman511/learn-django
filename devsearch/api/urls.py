b0c2ea859ee6c7af3e49e1bd035cd17cf7548413
Paul Bodner <bodner80@gmail.com>
2022-07-20 14:09:55 -0700

    implemented authentication tokens to allow vote on project from user

--- devsearch/api/urls.py
+++ devsearch/api/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
+from rest_framework_simplejwt.views import (
+  TokenObtainPairView,
+  TokenRefreshView,
+)
 from . import views
 
 urlpatterns = [
   path('', views.getRoutes),
   path('projects/', views.getProjects),
-  path('project/<str:pk>/', views.getProject)
+  path('project/<str:pk>/', views.getProject),
+  path('project/<str:pk>/vote', views.projectVote),
+
+  path('users/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+  path('users/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
