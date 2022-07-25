a85f77a661ba2b6def38fcf2a9c23400c552deea
Paul Bodner <bodner80@gmail.com>
2022-07-25 10:58:54 -0700

    implemented static file storage with Amazon web services s3 buckets

--- devsearch/devsearch/settings.py
+++ devsearch/devsearch/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'users.apps.UsersConfig',
     
     'rest_framework',
+    'storages',
 ]
 
 REST_FRAMEWORK = {
@@ -201,3 +202,11 @@ STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+AWS_QUERYSTRING_AUTH = False
+AWS_S3_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
+AWS_S3_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
+
+AWS_S3_HOST = 's3-website-us-west-2.amazonaws.com'
